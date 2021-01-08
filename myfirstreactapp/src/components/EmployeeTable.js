3a5a477031a96938927d2bfa12c20f989617101d
Timothy Forbes <timothy.forbes259@gmail.com>
2021-01-08 11:25:08 +1030

    reworked and functioning codebase

--- myfirstreactapp/src/components/EmployeeTable.js
+++ myfirstreactapp/src/components/EmployeeTable.js
@@ -1,43 +1,123 @@
+import React from 'react';
+
 import Table from "react-bootstrap/Table"
 
-const firstName = ["Patricia ", "Mikael", "Wayne", "Joaquin ", "Martine ", "Alan"]
-firstName.sort()
-console.log(firstName)
+/*The following code was copied from https://www.smashingmagazine.com/2020/03/sortable-tables-react/
+The guide was read through lost my way with the code and compared the following.
 
-function EmployeeTable(props) {
-  const { employees } = props;
-  let sortedFirstName = [...firstName]
-  sortedFirstName.sort((a, b) => {
-    if (a.name < b.name) {
-      return -1;
-     }
-    if (a.name > b.name) {
-      return 1;
+*/
+// function to look through and sort data 
+const useSortableData = (employeeData, config = null) => {
+  const [sortConfig, setSortConfig] = React.useState(config);
+
+  const sortedEmployeeData = React.useMemo(() => {
+    let sortableEmployeeData = [...employeeData];
+    //Depending on default, Ascending or Descending data     
+    if (sortConfig !== null) {
+      
+      //Ascending Conditional
+      sortableEmployeeData.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
+        }
+
+        //Descending Conditional
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? 1 : -1;
+        }
+        //Default
+        return 0;
+      });
     }
-    return 0;
-  });
-}
+    return sortableEmployeeData;
+  }, [employeeData, sortConfig]);
 
+  const requestSort = (key) => {
+    let direction = 'ascending';
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === 'ascending'
+    ) {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+  };
+
+  return { employeeData: sortedEmployeeData, requestSort, sortConfig };
+};
+
+const EmployeeTable = (props) => {
+  const { employeeData, requestSort, sortConfig } = useSortableData(props.employees);
+  const getClassNamesFor = (name) => {
+    if (!sortConfig) {
+      return;
+    }
+    return sortConfig.key === name ? sortConfig.direction : undefined;
+  };
   return (
-    <Table striped bordered hover variant="dark">
-      <caption>Our Employees</caption>
+    <table>
+      <caption>Employee Directory</caption>
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Age</th>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort('name')}
+              className={getClassNamesFor('name')}
+            >
+              Name
+            </button>
+          </th>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort('price')}
+              className={getClassNamesFor('price')}
+            >
+              Price
+            </button>
+          </th>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort('stock')}
+              className={getClassNamesFor('stock')}
+            >
+              In Stock
+            </button>
+          </th>
         </tr>
       </thead>
       <tbody>
-        {employees.map(employee => (
-          <tr key={employee.id}>
-            <td>{employee.name}</td>
-            <td>{employee.price}</td>
-            <td>{employee.stock}</td>
+        {employeeData.map((item) => (
+          <tr key={item.id}>
+            <td>{item.name}</td>
+            <td>${item.price}</td>
+            <td>{item.stock}</td>
           </tr>
         ))}
       </tbody>
-    </Table>
-  )
+    </table>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <EmployeeTable
+        employees={[
+          { id: 1, name: 'Cheese', price: 4.9, stock: 20 },
+          { id: 2, name: 'Milk', price: 1.9, stock: 32 },
+          { id: 3, name: 'Yoghurt', price: 2.4, stock: 12 },
+          { id: 4, name: 'Heavy Cream', price: 3.9, stock: 9 },
+          { id: 5, name: 'Butter', price: 0.9, stock: 99 },
+          { id: 6, name: 'Sour Cream ', price: 2.9, stock: 86 },
+          { id: 7, name: 'Fancy French Cheese 🇫🇷', price: 99, stock: 12 },
+        ]}
+      />
+    </div>
+  );
 }
-  export default EmployeeTable;
+
+
