58a243eedeccf751234581ab80c4cc48b9d99648
Timothy Forbes <timothy.forbes259@gmail.com>
2021-01-08 18:12:21 +1030

    rework after advice from course teaching assistant

--- myfirstreactapp/src/App.js
+++ myfirstreactapp/src/App.js
@@ -1,51 +1,17 @@
-//Bootstrap components
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 
-//My own components
-import EmployeeTable from "./components/EmployeeTable"
-
-import FilterInput from "./components/FilterInput"
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
-      <Container fluid>
-        <Row className="justify-content-md-center">
-          <h2>Employee Directory</h2>
-        </Row>
-        
-        <Row>
-          <Col xs lg="2"></Col>
-          <Col xs lg="2">
-            <FilterInput />
-          </Col>
-          <Col></Col>
-        </Row>
-
-        <Row className="justify-content-md-center">
-          <Col xs lg="3"></Col>
-          <Col xs lg ="6">
-            <EmployeeTable
-              employees={[
-                { id: 1, name: 'Patricia Millstein', age: 49, hobby: "Shadow Magic" },
-                { id: 2, name: 'Gaius Worzel', age: 19, hobby: "Horseback Riding" },
-                { id: 3, name: 'Charlie McBurn', age: 24, hobby: "Pyromanic" },
-                { id: 4, name: 'Dean Tsunatra', age: 42, hobby: "Singing" },
-                { id: 5, name: 'Bruce Constinata', age: 63, hobby: "Volunteering" },
-                { id: 6, name: 'Dina Beatrice', age: 45, hobby: "Painting" },
-                { id: 7, name: 'Hugo Belllingie', age: 99, hobby: "Investing" },
-            ]}/>
-          </Col>
-          <Col xs lg="3"></Col>
-        </Row>
-
-    </Container>
-
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
     </div>
   );
 }
